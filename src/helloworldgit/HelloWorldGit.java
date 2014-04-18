adf7ad655ca34c0476c9492678564ab306488c3a
VictorYu85 <victoryu85@gmail.com>
2014-04-17 22:04:36 -0700

    First file commit

---
+++ src/helloworldgit/HelloWorldGit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldgit;
+
+/**
+ *
+ * @author Vyu
+ */
+public class HelloWorldGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
